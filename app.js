899c3b466cd893cc107c41827128f4627a966f42
Wint Thanda Moe <wintthandamoe@gmail.com>
2023-07-14 12:11:24 +0630

    removepointer

--- app.js
+++ app.js
@@ -7,26 +7,36 @@ var circleidx = 0;
 
 getmap.addEventListener('click',function(e){
     circleidx++;
-    // console.log('hello')
-    const cx = e.clientX; //window ကနေကွာတဲ့ အကွာအဝေးကိုယူတာ
-    const cy = e.clientY;
-    // console.log(cx,cy);
+    // console.log(e.target)
+    if(e.target.classList.contains('map-container')){
+        const cx = e.clientX; //window ကနေကွာတဲ့ အကွာအဝေးကိုယူတာ
+        const cy = e.clientY;
+        // console.log(cx,cy);
 
-    const mapleft = e.target.offsetLeft; //မြေပုံရဲ့ လက်ရှိ ရှိနေတဲ့နေရာ
-    const maptop = e.target.offsetTop;
-    // console.log(mapleft,maptop);
+        const mapleft = e.target.offsetLeft; //မြေပုံရဲ့ လက်ရှိ ရှိနေတဲ့နေရာ
+        const maptop = e.target.offsetTop;
+        // console.log(mapleft,maptop);
 
-    const curx = cx - mapleft;
-    const cury = cy - maptop;
-    // console.log(curx,cury);
+        const curx = cx - mapleft;
+        const cury = cy - maptop;
+        // console.log(curx,cury);
 
-    const circle = document.createElement('span');
-    circle.id = circleidx;
-    circle.classList.add('circle');
-    console.log(circle);
+        const circle = document.createElement('span');
+        circle.id = circleidx;
+        circle.classList.add('circle');
+        // console.log(circle);
 
-    circle.style.left = `${curx}px`;
-    circle.style.top = `${cury}px`;
+        circle.style.left = `${curx}px`;
+        circle.style.top = `${cury}px`;
 
-    e.target.appendChild(circle);
+        e.target.appendChild(circle);
+    }
+    removepointer(e);
 })
+
+function removepointer(e){
+    // console.log(e.target.id)
+    if(e.target.id > 0){
+        e.target.remove();
+    }
+}
